4dba251e88129a0b073d8b49d487bfd6256441ea
Daanvanrhee <114082471+Daanvanrhee@users.noreply.github.com>
2023-12-17 21:06:17 +0000

    change

--- input.h
+++ input.h
@@ -4,10 +4,10 @@
 #include "arrivals.h"
 
 // the time in seconds it takes for a car to cross the intersection
-#define CROSS_TIME 5
+#define CROSS_TIME 1
 
 // the array of arrivals for the intersection
-const Arrival input_arrivals[] = {{0, NORTH, STRAIGHT, 0}, {1, SOUTH, LEFT, 1}, {2, EAST, STRAIGHT, 7}, {3, WEST, RIGHT, 13}};
+const Arrival input_arrivals[] = {{0, NORTH, STRAIGHT, 0}, {1, SOUTH, LEFT, 1}, {2, EAST, STRAIGHT, 1}, {3, WEST, RIGHT, 1},{2, EAST, STRAIGHT, 1}, {3, WEST, RIGHT, 1},{2, EAST, STRAIGHT, 7}, {3, WEST, RIGHT, 13},{2, EAST, STRAIGHT, 7}, {3, WEST, RIGHT, 13},{2, EAST, STRAIGHT, 7}, {3, WEST, RIGHT, 13},{2, EAST, STRAIGHT, 7}, {3, WEST, RIGHT, 13},{2, EAST, STRAIGHT, 7}, {3, WEST, RIGHT, 13}};
 
 
 #endif
